cedb4dd095875d84fa6bcc3390dbdcfd4291f528
Carlos Anriquez <canriquez@gmail.com>
2020-09-10 17:10:20 -0300

    install react/redux. installs heroku and basic test component

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <h1>
+                BookStore Heroku Test
+            </h1>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
