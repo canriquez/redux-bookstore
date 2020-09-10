754ff5d348761f6db4afe08647e2d32cac63d1aa
Carlos Anriquez <canriquez@gmail.com>
2020-09-10 17:55:40 -0300

    fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-
-class App extends React.Component {
-
-    render() {
-        return (
-            <h1>
-                BookStore Heroku Test
-            </h1>
-        )
-    }
-}
+const App = () => (
+  <h1>
+    BookStore Heroku Test
+  </h1>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
