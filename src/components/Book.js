e5bd8842584a836c0ae5b0ea8f34091395861fd0
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-09-14 15:24:34 -0500

    styling Book component on list-item and inner sections

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,29 +6,29 @@ const Book = ({
   book,
   handleRemoveBook,
 }) => (
-  <li>
-    <div>
-      <h3>{book.category}</h3>
-      <h1>{book.title}</h1>
-      <h2>Author here</h2>
+  <li className='list-item Lesson-Panel'>
+    <div className=''>
+      <h3 className='School-of'>{book.category}</h3>
+      <h1 className='title'>{book.title}</h1>
+      <h2 className='Suzanne-Collins'>Author here</h2>
+    </div>
+    <div className='book-options'>
+      <a href='#' className='book-option'>Comments</a>
+      {' '}
+      <a href='#' className='book-option' onClick={() => handleRemoveBook(book)}>Remove Book</a>
+      {' '}
+      <a href='#' className='book-option'>Comments</a>
     </div>
     <div>
-      <a divef='#'>Comments</a>
-      {' '}
-      <a divef='#' onClick={() => handleRemoveBook(book)}>Remove Book</a>
-      {' '}
-      <a divef='#'>Comments</a>
-    </div>
-    <div>
-      <div className='Oval-2'></div>
+      <div className='Rectangle-3'><div className='Oval-2'></div></div>
       <h1 className='-Percent-Complete'>64%</h1>
       <p className='Completed'>Completed</p>
     </div>
-    <div className='Lesson-Panel'></div>
+    <div className='Line-2'></div>
     <div>
-      <p className=''>Current chapter</p>
-      <p className=''>Chapter 17</p>
-      <button className=''>update progress</button>
+      <p className='Current-Chapter'>Current chapter</p>
+      <p className='Current-Lesson'>Chapter 17</p>
+      <button className='Rectangle-2'><p className='Update-progress'>update progress</p></button>
     </div>
   </li>
 
